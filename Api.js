63d60977e02a120e43313bf96dc78307c09e82cc
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2025-02-07 20:49:16 +0500

    Update Api.js

--- Api.js
+++ Api.js
@@ -3,7 +3,7 @@ const apikey ="792c3d464a0cc74abde03d9f4c375f1e&units=metric";
    // return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`)
     return await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`)
     .then((res) => res.json())
-    .then((json)=>{
+    .then((json)=>{ 
         return json;
     })
  }
